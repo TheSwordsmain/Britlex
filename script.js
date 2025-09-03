e2a4c81eace4b4d531d80b005b4cfb6f2b1e22cc
Asrorbek Gafforov <151524414+TheSwordsmain@users.noreply.github.com>
2025-09-03 22:48:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let knopka = document.querySelector('.header__btn')
+let aylana = document.querySelector(".circle");
+let hammasi = document.querySelector(".all");
+let tungi = document.querySelector(".nightmode");
+
+ aylana.addEventListener('click', function(){
+  hammasi.classList.toggle('all2');
+  tungi.classList.toggle('new');
+  aylana.classList.toggle('new1')
+  knopka.classList.toggle('newbtn')
+ });
+ tungi.addEventListener('click', function(){
+  hammasi.classList.toggle('all2');
+  tungi.classList.toggle('new');
+  aylana.classList.toggle('new1')
+  knopka.classList.toggle('newbtn')
+ });
+ aylana.addEventListener('click', function(){
+  hammasi.classList.toggle('all2');
+  tungi.classList.toggle('new');
+  aylana.classList.toggle('new1')
+  knopka.classList.toggle('newbtn')
+ });
+
